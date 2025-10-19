ea8b7b57e5932a2de33301fc12f2ebe981fcd9ba
xsanxxhez <sasha.agey1235@gmail.com>
2025-10-19 22:00:25 +0300

    com2 deploy frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
+    unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 export default nextConfig;
