198d9acb758bdf40c54bf4ea77589816c33ed83a
xsanxxhez <sasha.agey1235@gmail.com>
2025-10-28 20:33:17 +0300

    com38 - test2

--- backend/dist/src/main.js
+++ backend/dist/src/main.js
@@ -10,7 +10,7 @@ async function bootstrap() {
         origin: [
             'http://localhost:3000',
             'http://localhost:3001',
-            'https://borovy-frontend.onrender.com',
+            'https://borovy.of.by',
         ],
         credentials: true,
     });
